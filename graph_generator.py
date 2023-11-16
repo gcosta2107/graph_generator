7041178e3a6792c23de4fef8b73f03d084a14620
Gabriel Costa <80404513+gcosta2107@users.noreply.github.com>
2023-11-16 12:12:46 -0300

    Update grafo valorado - req. 4

--- graph_generator.py
+++ graph_generator.py
@@ -29,6 +29,12 @@ def print_menu_insercao():
             1. Inserir um a um
             2. Inserir em lote
         ''')
+
+def print_menu_valorado():
+    print('''
+            1. Grafo Valorado
+            2. Grafo Nao valorado
+        ''')
     
 def isDirecionado(direcionado):
     if(direcionado == 1):
@@ -52,38 +58,61 @@ def inserir_vertice(G):
         else:
             G.add_node(vertice)
 
-def inserir_aresta(G):
+def inserir_aresta_nao_valorada(G):
     while True:
-            
-            aresta1 = input("Digite o vertice inicial: ")    
-            aresta2 = input("Digite o vertice final: ")
-        
-            G.add_edge(aresta1, aresta2)
+        aresta1 = input("Digite o vertice inicial: ")    
+        aresta2 = input("Digite o vertice final: ")
+    
+        G.add_edge(aresta1, aresta2)
 
-            validator_aresta = input("Deseja adicionar mais arestas? [s/n]")
+        validator_aresta = input("Deseja adicionar mais arestas? [s/n]")
 
-            if(validator_aresta == 'n'):
-                break
+        if(validator_aresta == 'n'):
+            break
 
-def inserir_1a1(G, opcao_1a1):
+def inserir_aresta_valorada(G):
+    while True:
+        aresta1 = input("Digite o vertice inicial: ")    
+        aresta2 = input("Digite o vertice final: ")
+        peso = input("Digite o peso: ")
+
+        weighted_edges = [(aresta1, aresta2, peso)]
+        G.add_weighted_edges_from(weighted_edges)
+    
+        G.add_edge(aresta1, aresta2, weight = peso)
+
+        validator_aresta = input("Deseja adicionar mais arestas? [s/n]")
+
+        if(validator_aresta == 'n'):
+            break
+
+def inserir_1a1(G, opcao_1a1, modo_valorado):
     if(opcao_1a1 == 1):
         inserir_vertice(G)
 
     elif(opcao_1a1 == 2):
-        inserir_aresta(G)
+        if modo_valorado == 1:
+            inserir_aresta_valorada(G)
+        
+        if modo_valorado == 2:
+            inserir_aresta_nao_valorada(G)
 
 # def sao_adjacentes(grafo, vertice1, vertice2):
 #     return grafo.has_edge(vertice1, vertice2)
 
-menus = {'direcionado' : print_menu_direcionado, 'app' : print_menu_app, '1a1' : print_menu_1a1, 'insercao' : print_menu_insercao}
+menus = {'direcionado' : print_menu_direcionado, 'app' : print_menu_app, '1a1' : print_menu_1a1,
+        'insercao' : print_menu_insercao, 'valorado' : print_menu_valorado}
 
 menus["direcionado"]()
 modo_direcionado = int(input("Opcao: "))
 
-G = isDirecionado(modo_direcionado)
-
 menus["insercao"]()
 modo_insercao = int(input("Opcao: "))
+
+menus["valorado"]()
+modo_valorado = int(input("Opcao: "))
+
+G = isDirecionado(modo_direcionado)
 
 opcao = 999
 
@@ -101,13 +130,16 @@ while opcao != 0:
 
         opcao_1a1 = int(input("Opcao: "))
 
-        inserir_1a1(G, opcao_1a1)     
+        inserir_1a1(G, opcao_1a1, modo_valorado)     
 
     if(opcao == 2):
         continue
 
     if(opcao == 3):
         nx.draw(G, with_labels=True)
+        pos = nx.spring_layout(G)
+        labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
         plt.show()
 
     if(opcao == 4):
