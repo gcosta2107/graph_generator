afca3d553050a762384664850c823fec7223b663
gcosta2107 <garragr10@gmail.com>
2023-11-16 17:56:46 -0300

    add leitura em lote - req. 3

--- graph_generator.py
+++ graph_generator.py
@@ -10,21 +10,20 @@ def print_menu_direcionado():
     
 def print_menu_app():
     print('''
-            1. Inserir um a um [VERTICES/ARESTAS]
-            2. Inserir em lote
+            1. Inserir elementos [VERTICES/ARESTAS]
+            2. ----------------
             3. Exibir grafo
             4. Informacoes do grafo
             0. Sair
         ''')
     
-def print_menu_1a1():
+def print_menu_inserir_elementos():
     print('''
             1. Inserir vertice
             2. Inserir aresta
-            0. Sair
         ''')
     
-def print_menu_insercao():
+def print_menu_modo_insercao():
     print('''
             1. Inserir um a um
             2. Inserir em lote
@@ -32,8 +31,8 @@ def print_menu_insercao():
 
 def print_menu_valorado():
     print('''
-            1. Grafo Valorado
-            2. Grafo Nao valorado
+            1. Aresta Valorada
+            2. Aresta Nao valorada
         ''')
     
 def isDirecionado(direcionado):
@@ -86,68 +85,105 @@ def inserir_aresta_valorada(G):
         if(validator_aresta == 'n'):
             break
 
-def inserir_1a1(G, opcao_1a1, modo_valorado):
+def inserir_elemento(G, opcao_1a1, modo_valorado):
     if(opcao_1a1 == 1):
         inserir_vertice(G)
 
-    elif(opcao_1a1 == 2):
+    if(opcao_1a1 == 2):
         if modo_valorado == 1:
             inserir_aresta_valorada(G)
         
         if modo_valorado == 2:
             inserir_aresta_nao_valorada(G)
+    
+
+def inserir_em_lote(G, modo_valorado, arquivo):
+    try:
+        with open(arquivo, 'r') as file:
+            for line in file:
+                dados = line.strip().split()
+
+                if len(dados) < 2:
+                    print("Formato inválido na linha:", line)
+                else:
+                    vertice1, vertice2 = dados[0], dados[1]
+
+                    if len(dados) == 3:
+                        modo_valorado == 2 
+                        peso = dados[2]
+                        G.add_edge(vertice1, vertice2, weight=peso)
+                    else:
+                        G.add_edge(vertice1, vertice2)
+
+        print("Inserção em lote concluída.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+    except Exception as e:
+        print("Ocorreu um erro durante a leitura do arquivo:", e)
 
 # def sao_adjacentes(grafo, vertice1, vertice2):
 #     return grafo.has_edge(vertice1, vertice2)
 
-menus = {'direcionado' : print_menu_direcionado, 'app' : print_menu_app, '1a1' : print_menu_1a1,
-        'insercao' : print_menu_insercao, 'valorado' : print_menu_valorado}
+menus = {'isDirecionado' : print_menu_direcionado, 'menu_principal' : print_menu_app, 'inserir_elemento' : print_menu_inserir_elementos,
+        'modo_insercao' : print_menu_modo_insercao, 'isValorado' : print_menu_valorado}
 
-menus["direcionado"]()
-modo_direcionado = int(input("Opcao: "))
+if __name__ == "__main__":
+    menus["isDirecionado"]()
+    modo_direcionado = int(input("Opcao: "))
 
-menus["insercao"]()
-modo_insercao = int(input("Opcao: "))
+    G = isDirecionado(modo_direcionado)
 
-menus["valorado"]()
-modo_valorado = int(input("Opcao: "))
+    opcao = 999
 
-G = isDirecionado(modo_direcionado)
+    modo_valorado = 0
 
-opcao = 999
+    while opcao != 0:
+        
+        menus["menu_principal"]()
 
-while opcao != 0:
-    
-    menus["app"]()
+        opcao = int(input("Opcao: "))
 
-    opcao = int(input("Opcao: "))
+        if(opcao == 0):
+            print("Encerrando Aplicacao....")
 
-    if(opcao == 0):
-        print("Encerrando Aplicacao....")
+        if(opcao == 1):
+            menus["modo_insercao"]()
+            modo_insercao = int(input("Opcao: "))
 
-    if(opcao == 1):
-        menus["1a1"]()
+            if modo_insercao == 1:
+                menus["inserir_elemento"]()
+                opcao_elemento = int(input("Opcao: "))
 
-        opcao_1a1 = int(input("Opcao: "))
+                if opcao_elemento == 1:
+                    modo_valorado = 0
 
-        inserir_1a1(G, opcao_1a1, modo_valorado)     
+                if opcao_elemento == 2:
+                    menus["isValorado"]()
+                    modo_valorado = int(input("Opcao: "))
 
-    if(opcao == 2):
-        continue
+                inserir_elemento(G, opcao_elemento, modo_valorado)
 
-    if(opcao == 3):
-        nx.draw(G, with_labels=True)
-        pos = nx.spring_layout(G)
-        labels = nx.get_edge_attributes(G, 'weight')
-        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
-        plt.show()
+            if modo_insercao == 2:
+                print("ATENÇÃO!!! O ARQUIVO DEVE ESTAR NO FORMATO (VERTICE1 VERTICE2 PESO (se for o caso))")
+                arquivo = input("Digite o nome do arquivo: ")
+                inserir_em_lote(G, modo_valorado, arquivo)
 
-    if(opcao == 4):
-        ordem = G.order()
-        print("Ordem do Grafo:", ordem)
+        if(opcao == 2):
+            continue
 
-        tamanho = G.size()
-        print("Tamanho do Grafo:", tamanho)
+        if(opcao == 3):
+            nx.draw(G, with_labels=True)
+            pos = nx.spring_layout(G)
+            labels = nx.get_edge_attributes(G, 'weight')
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+            plt.show()
 
-print(G.nodes)
-print(G.edges)
+        if(opcao == 4):
+            ordem = G.order()
+            print("Ordem do Grafo:", ordem)
+
+            tamanho = G.size()
+            print("Tamanho do Grafo:", tamanho)
+
+    print(G.nodes)
+    print(G.edges)
